acf56f1a82c39077270b64827d90acaa67a70e23
Benjamin Summers <bsummer4@eecs.utk.edu>
2013-11-12 01:40:04 +0000

    Messing around with Haskell's unicode support.

--- CL.hs
+++ CL.hs
@@ -1,28 +1,30 @@
+{-# LANGUAGE UnicodeSyntax #-}
+
 module CL(main) where
 import OL
 import Edit
 import System.IO
 
-red :: String -> String
+red ∷ String → String
 red s = "\ESC[1;31m" ++ s ++ "\ESC[0m"
 
-pretty :: State -> String
+pretty ∷ State → String
 pretty s@(State a ol) = olshow ol' where
 	(State _ ol') = apply (Edit $ ols $ red $ unols $ getNode s) s
 
-writeScreen :: String -> IO()
+writeScreen ∷ String → IO()
 writeScreen s = putStr "\ESC[2J" >> putStr s
 
-inputloop :: State -> IO()
+inputloop ∷ State → IO()
 inputloop st@(State a ol) = do
 	writeScreen $ pretty st
 	cmd <- getLine
 	inputloop $ apply (parseCmd cmd) st
 
-main :: IO()
+main ∷ IO()
 main = inputloop (State (Addr[]) olexample)
 
-parseCmd :: String -> Edit.Mut
+parseCmd ∷ String → Edit.Mut
 parseCmd "h" = SelLeft
 parseCmd "j" = SelDown
 parseCmd "k" = SelUp
--- Test.hs
+++ Test.hs
@@ -1,6 +1,9 @@
+{-# LANGUAGE UnicodeSyntax #-}
+
 module Test where
 import Test.QuickCheck
 import Data.List
 import OL
+import Prelude.Unicode
 
-main = quickCheck ((\i is -> i==head(i:is)) :: Int -> [Int] -> Bool)
+main = quickCheck ((\i is→i≡head(i:is))∷Int→[Int]→Bool)
