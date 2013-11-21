04c6de15a576e87a42d598bf901e2d4cfb09565e
Benjamin Summers <bsummer4@eecs.utk.edu>
2013-11-21 06:21:31 +0000

    No longer store or query any information in the DOM.

--- UI.hs
+++ UI.hs
@@ -4,48 +4,71 @@ import FFI
 import JS
 import Util
 import OL
-import DOM
 import Edit
 import Editor
 import FayRef
 
--- Manipulate the State Variable -----------------------------------------------
-setSel st a = modifyFayRef st (\(State _ o) -> State a o)
-getSel st = readFayRef st >>= (\(State a _) -> return a)
-setOutline st o = modifyFayRef st (\(State a _) -> State a o)
-getOutline st = readFayRef st >>= (\(State _ o) -> return o)
-getState st = readFayRef st
-setState st state = modifyFayRef st (\_ -> state)
+-- DOM ------------------------------------------------------------------------
+data DOM = Node
+	{ tag :: String
+	, attrs :: [(String,String)]
+	, text :: Maybe String
+	, click :: Maybe(JSDOM -> Fay())
+	, childs :: [DOM] }
 
--- DOM -------------------------------------------------------------------------
-setAttrs n [] = return ()
+dom :: String -> DOM
+dom s = Node {tag=s, attrs=[], text=Nothing, click=Nothing, childs=[]}
+(dP,dSPAN,dUL,dLI,dPRE) = (dom "p", dom "span", dom "ul", dom "li", dom "pre")
+
+olDom :: FayRef State -> Addr -> OL -> DOM
+olDom st selection tree = top tree where
+	l a txt = dSPAN{attrs=[cls a],text=Just(unols txt),click=Just(select st a)}
+	cls a = ("class",if a==selection then "selected" else "unselected")
+	r a (OL txt []) = [ dLI{childs=[l a txt]} ]
+	r a (OL txt cs) = [ dLI{childs=[l a txt]}, dUL{childs=concat$addrmap a r cs} ]
+	top (OL txt []) = dP{childs=[l (Addr[]) txt]}
+	top (OL txt cs) = dP{childs=
+		[ l (Addr[]) txt
+		, dUL{childs=concat$addrmap (Addr[]) r cs} ]}
+
+setAttrs :: JSDOM -> [(String,String)] -> Fay()
+setAttrs _ [] = return ()
 setAttrs n ((k,v):as) = setAttr n k v >> setAttrs n as
+
+getOutlineNode :: Fay JSDOM
 getOutlineNode = gid "outline" >>= (\ol -> case ol of
 	Null -> error "There has to be a node with id=“outline” in the document."
 	Nullable node -> return node)
 
+clearPage :: Fay()
 clearPage = do {o<-getOutlineNode; getChilds o >>= iter (removeChild o)}
+
+writePage :: JSDOM -> Fay()
 writePage d = getOutlineNode >>= (\o -> appendChild o d)
+
+setPage :: JSDOM -> Fay()
 setPage x = clearPage >> writePage x
-gendom (Node tag attrs txt childs) = do
-	n <- mknode tag
-	setAttrs n attrs
-	case txt of {Nothing->return (); Just s->setText n s}
-	mymapM gendom childs >>= iter (appendChild n)
+
+gendom :: DOM -> Fay JSDOM
+gendom d = do
+	n <- mknode (tag d)
+	setAttrs n (attrs d)
+	case (text d) of {Nothing->return (); Just s->setText n s}
+	mymapM gendom (childs d) >>= iter (appendChild n)
+	case (click d) of {Nothing->return(); Just c->onClick n (c n)}
 	return n
 
--- Main ------------------------------------------------------------------------
+-- Main -----------------------------------------------------------------------
+prompt' :: String -> String -> Fay String
 prompt' q default' = do
 	x <- prompt q default'
 	case x of {Null->return default'; Nullable t->return t}
 
-nodeAddr n = getId n >>= r where
-	r Null = error "Internal Error: Clickable node has invalid ID"
-	r (Nullable i) = return(idAddr i)
+select :: FayRef State -> Addr -> JSDOM -> Fay()
+select st a _ = modifyFayRef st (\(State _ o) -> State a o) >> buildit st
 
-select st n = nodeAddr n >>= setSel st >> buildit st
-editKey n k = do
-	t <- getText n
+editKey :: String -> String -> Fay Mut
+editKey t k = do
 	case k of
 		"h" -> return SelLeft
 		"l" -> return SelRight
@@ -60,29 +83,29 @@ editKey n k = do
 		"\r" -> prompt' "Replace Text" t >>= return . ReplaceTxt . ols
 		_ -> return Nada
 
+setupKeys :: (FayRef State) -> Fay()
 setupKeys st = onKeyPress r where
 	dumpText t = gendom payload >>= writePage where
-		payload = Node "pre" [] (Just $ t++"\n") []
-	r "!" = getState st >>= (\(State a ol) -> dumpText$olshow$ol)
+		payload = dPRE{ text=Just $ t++"\n" }
+	r "!" = readFayRef st >>= (\(State _ ol) -> dumpText$olshow$ol)
 	r k = do
-		a <- getSel st
-		s <- getState st
-		n' <- gid $ addrId a
-		n <- return $ case n' of {Null->error "bad addr"; Nullable z->z}
-		op <- editKey n k
-		setState st $ apply op s
+		s <- readFayRef st
+		op <- editKey "TODO Get the currently selected text" k
+		writeFayRef st $ apply op s
 		buildit st
 
+fixAddr :: (FayRef State) -> Fay()
 fixAddr st = readFayRef st >>= \(State addr outline) ->
 	if validSel addr outline then return() else
 		modifyFayRef st (\(State _ o) -> State (Addr[]) o)
 
+buildit :: (FayRef State) -> Fay()
 buildit st = do
 	fixAddr st
 	State a ol <- readFayRef st
-	gendom(olDom a ol) >>= setPage
-	byClass "unselected" >>= iter (\e -> onClick e (select st e))
+	gendom(olDom st a ol) >>= setPage
 
+main :: Fay()
 main = do
 	state <- newFayRef $ State (Addr[]) olexample
 	buildit state
