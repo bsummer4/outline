954509eef373d37256c6b39fc1734d85ca1839e2
Benjamin Summers <bsummer4@eecs.utk.edu>
2013-12-03 12:48:58 +0000

    First stab at reworking ‘Sync.hs’. It's hairy and breaks ‘Test.hs’.

--- Sync.hs
+++ Sync.hs
@@ -1,41 +1,21 @@
--- Syncronization Model:
-	-- Messages are released only when we have all information for a chunk of time.
-		-- all messages f
-	-- Idle clients regularly send TRACE messages.
-	-- If messages are delivered, they are intact.
-	-- Messages may not be delivered, so we need to know if we missed one.
-
--- In a Queue,
-	-- nextId is the id that the next new client will be assigned, and also
-		-- the number of active cleints.
-	-- lastSync is a timestamp such that
-		-- ¬∃ a message m yeilded by Advance gives (ts m>lastSync).
-		-- ∃ a message m yeilded by Advance that gives (ts m≡lastSync || -1≡lastSync)
-
 module Sync where
 	-- ( Queue, Msg, TimeStamp, ClientId
 	-- , msg, epoch, tsInc, qEmpty
 	-- , qConnect, qMsg, qMsgs
 	-- , prop_flushable
 	-- ) where
+
 import Util
 import Data.List (sort)
 
+-- Maintain a list of messages from each client.
+-- When we get a TRACE message. Produce a chunk of messages.
+
 -- Data Types
 newtype TimeStamp = TS Int deriving (Ord,Show,Eq)
-newtype ClientId = ID Int deriving (Ord,Show,Eq)
-
-data Msg a
-	= MSG { ts::TimeStamp, user::ClientId, payload::a }
-	| TRACE {traceTime::TimeStamp, lastTrace::Maybe TimeStamp, msgsThisTime::Int}
-	deriving (Show,Ord,Eq)
-
-data Queue a = Queue
-	{ nextId :: ClientId
-	, lastSync :: TimeStamp
-	, pending :: [Msg a]
-	, connected :: [ClientId]
-	} deriving (Show,Eq)
+data Msg dat = TRACE TimeStamp | MSG dat deriving (Show,Ord,Eq)
+data Queue dat usr = Queue TimeStamp [(usr,[Msg dat])]
+	deriving (Show,Eq)
 
 -- API
 epoch :: TimeStamp
@@ -44,50 +24,51 @@ epoch = TS 0
 tsInc :: TimeStamp -> TimeStamp
 tsInc (TS i) = TS $ i+1
 
-qEmpty :: Queue a
-qEmpty = Queue { nextId=ID 0, lastSync=TS(-1), pending=[], connected=[] }
+qEmpty :: Queue a b
+qEmpty = Queue (TS(-1)) []
 
-msg :: TimeStamp -> ClientId -> a -> Msg a
-msg = MSG
+qConnect :: Eq usr => Queue dat usr -> usr -> Maybe(Queue dat usr)
+qConnect (Queue l p) id = case lookup id p of
+	Just _ -> Nothing
+	Nothing -> Just $ Queue l ((id,[]):p)
 
-qDisconnect :: Queue a -> ClientId -> Maybe(Queue a)
-qDisconnect (Queue n l p c) id = if c==c' then Nothing else Just q' where
-	q' = Queue n l p' c'
-	p' = filter ((/= id) . user) p
-	c' = filter (/= id) c
-
-qConnect :: Queue a -> (ClientId,Queue a)
-qConnect (Queue (ID n) l p c) = (ID n,Queue (ID $ n+1) l p (ID n:c))
-
-qMsg :: Queue a -> Msg a -> Maybe (Queue a,[Msg a])
-qMsg q m = mmap qAdvance $ qAddMsg q m
-
-qMsgs :: Show a => Queue a -> [Msg a] -> Maybe (Queue a,[Msg a])
-qMsgs queue ms = mmap qAdvance $ foldl f (Just queue) ms where
-	f Nothing _ = error "this should never happen."
-	-- f (Just q) m = qAddMsg q m
-	f (Just q) m = case qAddMsg q m of
-		Nothing -> error $
-			"Fucked up\nmsg:\n\t(" ++ show m ++ ")\non queue:\n\t(" ++ show q ++ ")"
-		Just a -> Just a
+-- TODO We might lose info by dropping pending messages that others have
+-- already incorperated.
+qDisconnect :: Eq usr => Queue dat usr -> usr -> Maybe(Queue dat usr)
+qDisconnect (Queue l p) id = mmap f $ lookup id p where
+	f _ = Queue l $ filter (\(i,_) -> i==id) p
 
 -- Internal
-qAddMsg :: Queue a -> Msg a -> Maybe(Queue a)
-qAddMsg (Queue clients sync q c) m@(MSG t id a) =
-	if id<clients && t>sync then Just $ Queue clients sync (m:q) c else Nothing
+-- TODO This implementation is incredably naive.
+qMsg :: Eq usr => Queue dat usr -> usr -> Msg dat -> Maybe(Queue dat usr)
+qMsg (Queue l p) u m@(MSG d) = case lookup u p of
+	Nothing -> Nothing
+	Just up -> Just $ Queue l $ (u,m:up):p
 
-qAdvance :: Queue a -> (Queue a,[Msg a])
-qAdvance q@(Queue _ _ [] _) = (q,[])
-qAdvance q@(Queue (ID 0) _ _ _) = (q,[])
-qAdvance q@(Queue (ID clients) old msgs connected) = r where
-	r = (Queue (ID clients) sync keep connected, yeild)
-	TS new = minimum $ map (maxTS . byClient msgs) $ connected
-	sync = TS(new-1)
-	keep = filter (\m -> ts m > sync) msgs
-	yeild = filter (\m -> ts m <= sync) msgs
-	byClient ms i = filter (\m -> i==user m) ms
+qAdvance :: Queue dat usr -> Maybe (Queue dat usr,[dat])
+qAdvance q@(Queue l p) = case rawr q of
+	Nothing -> Nothing
+	Just wat -> Just (q',yeild) where
+		q' = Queue (tsInc l) $ map (\(u,(k,y)) -> (u,k)) wat
+		yeild = concat $ map (\(u,(_,y)) -> y) wat
 
-maxTS :: [Msg a] -> TimeStamp
-maxTS [] = TS(-1) -- TODO Hack!
-maxTS (m:ms) = ts $ foldl f m ms where
-	f m1 m2 = if ts m1 > ts m2 then m1 else m2
+rawr :: Queue dat usr -> Maybe [(usr,([Msg dat],[dat]))]
+rawr q@(Queue l p) = foldl unMaybe (Just[]) xyz where
+	targetTime = tsInc l
+	xyz = map (\(u,ms) -> (u,advanceUsr targetTime [] ms)) p
+	unMaybe Nothing _ = Nothing
+	unMaybe _ (_,Nothing) = Nothing
+	unMaybe (Just a) (u,Just e) = Just ((u,e):a)
+
+advanceUsr :: TimeStamp -> [Msg d] -> [Msg d] -> Maybe([Msg d],[d])
+advanceUsr targetTime = a where
+	a _ [] = Nothing
+	a acc (MSG d:ms) = a (MSG d:acc) ms
+	a acc (m@(TRACE t):ms) =
+		if t/=targetTime then a (m:acc) ms else
+			case foldl collect (Just[]) ms of
+				Nothing -> Nothing
+				Just yeild -> Just(reverse acc, yeild)
+	collect _ (TRACE _) = Nothing
+	collect Nothing _ = Nothing
+	collect (Just acc) (MSG d) = Just(d:acc)
